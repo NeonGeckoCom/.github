d0884f4a115997dd3fdb87425ef9c52f518bb584
Daniel McKnight <daniel@neongecko.com>
2023-03-02 14:38:28 -0800

    Add debugging info to troubleshoot CommonPlay test failures

--- test/test_skill_resources.py
+++ test/test_skill_resources.py
@@ -98,7 +98,8 @@ class TestSkillLoading(unittest.TestCase):
     def test_skill_setup(self):
         self.assertEqual(self.skill.skill_id, self.test_skill_id)
         for msg in self.messages:
-            self.assertEqual(msg["context"]["skill_id"], self.test_skill_id)
+            self.assertEqual(msg["context"]["skill_id"], self.test_skill_id,
+                             msg.serialize())
 
     def test_intent_registration(self):
         registered_adapt = list()
