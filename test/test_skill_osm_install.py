80a00d84c63535620e2adc9a3c5ef42187f04b4e
Daniel McKnight <daniel@neongecko.com>
2023-05-08 15:21:38 -0700

    Fix typo in import

--- test/test_skill_osm_install.py
+++ test/test_skill_osm_install.py
@@ -27,7 +27,7 @@
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import unittest
-import pytest.mock
+import pytest.mark
 
 from os import environ
 from os.path import exists, isdir, isfile, join
@@ -38,7 +38,7 @@ from ovos_skills_manager import SkillEntry
 
 class TestOSM(unittest.TestCase):
     # TODO: Remove with next OSM release or deprecate test
-    @pytest.mark.skip(reason="OSM Dependency Bug with `requests`")
+    @pytest.mark.xfail(reason="OSM Dependency Bug with `requests`")
     def test_osm_install(self):
         branch = environ.get("TEST_BRANCH")
         install_url = f"https://github.com/{environ.get('TEST_REPO')}@{branch}"
